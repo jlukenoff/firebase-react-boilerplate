8621b97b8dcfdb58c2e16a34fa861a689d9d9e14
John Lukenoff <jlukenoff@gmail.com>
2021-10-02 17:33:31 -0700

    Server setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,8 @@
-import functions from "firebase-functions";
+import * as functions from "firebase-functions";
+import express from "express";
 
-functions.ad;
+const app = express();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(express.static("public"));
+
+exports.app = functions.https.onRequest(app);
